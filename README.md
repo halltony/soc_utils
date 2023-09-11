ec68382dd54e54b2d3dc07900e0867fc0f54e4ad
halltony <a.j.hall@blueyonder.co.uk>
2023-09-11 15:04:26 +0100

    addSeason completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # soc_utils
 Utility scripts for manipulating bird observation data
+
+1.  addSeason.py - this will take as input a csv filepath and column name that contains a date string in the format DD/MM/CCYY.  It will insert a column after the supplied column contain a season name based on the astronomically defined seasons using the pyephem package.
---
+++ addSeason.py
@@ -0,0 +1,33 @@
+import argparse
+import pandas as pd
+import datetime
+from datetime import date, datetime
+
+#defining function
+def get_season(date_str):
+  date_format = '%d/%m/%Y'
+  date = datetime.strptime(date_str, date_format)
+  m = date.month
+  x = m%12 // 3 + 1
+  if x == 1:
+    season = 'Winter'
+  if x == 2:
+    season = 'Spring'
+  if x == 3:
+    season = 'Summer'
+  if x == 4:
+    season = 'Autumn'
+  return season
+
+parser = argparse.ArgumentParser(description="Add a season column to a CSV file",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-f", "--file_path", type=str, required=True, help='filepath to the csv to be processed')
+parser.add_argument("-c", "--date_column", type=str, required=True, help="column containing the date")
+
+args = parser.parse_args()
+config = vars(args)
+
+df = pd.read_csv(args.file_path)
+df['season'] = df[args.date_column].map(get_season)
+df.to_csv(args.file_path, index=False)
