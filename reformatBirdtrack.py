d020e2944a9b93682e435555c702e9d5d90f4c5f
halltony <a.j.hall@blueyonder.co.uk>
2023-10-09 16:43:53 +0100

    Updates to reformat script

--- reformatBirdtrack.py
+++ reformatBirdtrack.py
@@ -6,48 +6,42 @@ import datetime
 from datetime import date, datetime
 import shutil
 
-
-
 #defining function
-def get_season(date_str):
-  date_format = '%d/%m/%Y'
-  date = datetime.strptime(date_str, date_format)
-  m = date.month
-  x = m%12 // 3 + 1
-  if x == 1:
-    season = 'Winter'
-  if x == 2:
-    season = 'Spring'
-  if x == 3:
+def get_season(dateTime):
+  m = dateTime.month
+  d = dateTime.day
+  if (m < 5 and d < 16):
+    season = 'Winter/Spring'
+  elif m < 8:
     season = 'Summer'
-  if x == 4:
-    season = 'Autumn'
+  else:
+    season = 'Autumn/Winter'
   return season
 
 parser = argparse.ArgumentParser(description="Reformat Birdtrack Data for Annual Report",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-f", "--file_path", type=str, required=True, help='filepath to the csv to be processed')
+parser.add_argument("-f", "--file_path", type=str, required=True, help='Filepath to the Excel file to be processed')
 
 
 args = parser.parse_args()
 config = vars(args)
 
 #Save a backup of the original file
-backup = args.file_path.replace('.csv', '.bak')
+backup = args.file_path.replace('.xlsx', '.bak')
 shutil.copyfile(args.file_path, backup)
 
-df = pd.read_csv(args.file_path)
+df = pd.read_excel(args.file_path, converters= {'Date': pd.to_datetime})
 
 # Remove columns that are not required
-columnsToRemove = ['BTO species code', 'BOU order', 'Scientific name', 'Grid reference', 'Uncertainty radius',
+columnsToRemove = ['BTO species code', 'BOU order', 'Grid reference', 'Uncertainty radius',
                    'Geometry type', 'Lat', 'Long', 'Pinpoint', 'Observer name', 'User ID', 'User name', 
                    'Start time', 'End time']
 
 df = df.drop(columnsToRemove, axis=1)
 
 # Reorder the remaining columns
-requiredColumnOrder = ['Species', 'Count', 'Place', 'Date', 'Comment']
+requiredColumnOrder = ['Species', 'Scientific name', 'Count', 'Place', 'Date', 'Comment']
 df = df.reindex(columns=requiredColumnOrder)
 
 # Insert a season column
@@ -56,7 +50,7 @@ df['Season'] = df['Date'].map(get_season)
 # Sort by date order within species - this requires converting to a datetime object
 df['Date'] = pd.to_datetime(df.Date, dayfirst=True)
 df = df.sort_values(['Species','Date'],ascending=[True,True])
-df['Date'] = df['Date'].dt.strftime('%d/%m/%Y')
+df['Date'] = df['Date'].dt.strftime('%d %b')
 
 # Overwrite the file
-df.to_csv(args.file_path, index=False)
+df.to_excel(args.file_path, index=False)
