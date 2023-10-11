3ba0761a6e9d4bf42411ea5cd793d933b879e31b
halltony <a.j.hall@blueyonder.co.uk>
2023-10-11 18:51:31 +0100

    Process all seasons

---
+++ analysis.py
@@ -0,0 +1,25 @@
+#!/Users/Tony/.pyenv/shims/python
+
+import argparse
+import pandas as pd
+import datetime
+from datetime import date, datetime
+
+parser = argparse.ArgumentParser(description="Analyse Birdtrack Data for Annual Report",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-f", "--file_path", type=str, required=True, help='Filepath to the Excel file to be processed')
+
+
+args = parser.parse_args()
+config = vars(args)
+
+df = pd.read_excel(args.file_path, converters= {'Date': pd.to_datetime})
+
+print('Number of species recorded: ' + str(df['Species'].nunique()))
+
+print('Total number of records: ' + str(df.shape[0]))
+
+pd.set_option('display.max_rows', 300)
+print('Number of records by Species')
+print( df['Species'].value_counts() )
--- createWordDoc.py
+++ createWordDoc.py
@@ -15,6 +15,25 @@ import argparse
 import pandas as pd
 import numpy as np
 
+def process_season(season, document, season_df):
+    if len(season_df) > 0:
+        p = document.add_paragraph('')
+        p.add_run(season).bold = True
+        table = document.add_table(rows=1, cols=4)
+        # Create the table headings
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = 'Count'
+        hdr_cells[1].text = 'Place'
+        hdr_cells[2].text = 'Date'
+        hdr_cells[3].text = 'Comment'
+        for index, row in season_df.iterrows():
+            row_cells = table.add_row().cells
+            row_cells[0].text = str(row['Count'])
+            row_cells[1].text = row['Place']
+            row_cells[2].text = row['Date']
+            if row['Comment'] != 0:
+                row_cells[3].text = row['Comment']
+
 parser = argparse.ArgumentParser(description="Create Bird Report from a reformated Birdtrack export",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -33,32 +52,22 @@ speciesList = df.Species.unique()
 
 for species in speciesList:
     species_df = df[df['Species'] == species]
-    scientificName = species_df['Scientific name'].unique()
-    p = document.add_paragraph('')
-    p.add_run(species).bold = True
-    p.add_run(' ')
-    p.add_run(scientificName).italic = True
-
-    season_df = species_df[species_df['Season'] == 'Winter/Spring']
-
-    if len(season_df) > 0:
+    if len(species_df) < 50 and not species.startswith('Unidentified'):
+        scientificName = species_df['Scientific name'].unique()
         p = document.add_paragraph('')
-        p.add_run('Winter/Spring:').bold = True
-        table = document.add_table(rows=1, cols=4)
-        # Create the table headings
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = 'Count'
-        hdr_cells[1].text = 'Place'
-        hdr_cells[2].text = 'Date'
-        hdr_cells[3].text = 'Comment'
-        for index, row in season_df.iterrows():
-            row_cells = table.add_row().cells
-            row_cells[0].text = str(row['Count'])
-            row_cells[1].text = row['Place']
-            row_cells[2].text = row['Date']
-            if row['Comment'] != 0:
-                row_cells[3].text = row['Comment']
+        p.add_run(species).bold = True
+        p.add_run(' ')
+        p.add_run(scientificName).italic = True
 
-    document.add_page_break()
+        season_df = species_df[species_df['Season'] == 'Winter/Spring']
+        process_season('Winter/Spring', document, season_df)
+
+        season_df = species_df[species_df['Season'] == 'Summer']
+        process_season('Summer', document, season_df)
+
+        season_df = species_df[species_df['Season'] == 'Autumn/Winter']
+        process_season('Autumn/Winter', document, season_df)
+
+        document.add_page_break()
 
 document.save('demo.docx')
